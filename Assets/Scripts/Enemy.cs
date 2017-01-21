e24c37f4577052ee3284f3e81b117e5060972c36
Jacudibu <jacudibu@gmail.com>
2017-01-21 17:14:05 +0100

    Added Enemies and stuff

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyColor
+{
+    RED,
+    GREEN,
+    BLUE,
+}
+
+public class Enemy : MonoBehaviour
+{
+    public Enemy parentEnemy;
+
+    [Range(0.5f, 5f)]
+    public float speed = 1f;
+
+	void Update ()
+    {
+        transform.Translate(0f, -speed * Time.deltaTime, 0f);
+	}
+
+    public int GetEnemyParentCount()
+    {
+        if (parentEnemy == null)
+        {
+            return 0;
+        }
+        else
+        {
+            return parentEnemy.GetEnemyParentCount();
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Vector3 spawnPointCenter;
+    public Vector3 spawnBoxSize;
+
+    private float delayBetweenEnemies = 3f;
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delayBetweenEnemies);
+            SpawnNewEnemy();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(spawnPointCenter, spawnBoxSize);
+    }
+
+    private void SpawnNewEnemy()
+    {
+        Vector3 pos = new Vector3();
+
+        pos.x = Random.Range(spawnPointCenter.x - spawnBoxSize.x * 0.5f, spawnPointCenter.x + spawnBoxSize.x * 0.5f);
+        pos.y = Random.Range(spawnPointCenter.y - spawnBoxSize.y * 0.5f, spawnPointCenter.y + spawnBoxSize.y * 0.5f);
+
+        GameObject.Instantiate(enemyPrefab, pos, Quaternion.identity);
+    }
+}

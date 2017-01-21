5c58a43f1797a00ac3a27e4583d2f8776d822b60
unknown <mail@tomgon.de>
2017-01-21 19:17:31 +0100

    size wobble

---
+++ Assets/Scripts/SineSize.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SineSize : MonoBehaviour
+{
+
+	public float SizeSpeed = 1;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.localScale = Vector3.one * (1f + Mathf.Sin(Time.time * SizeSpeed) * 0.2f);
+	
+	}
+}

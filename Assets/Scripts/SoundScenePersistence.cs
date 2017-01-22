fe0fbd8b44e740c7afe051df46d2aa7a1c383c2f
unknown <mail@tomgon.de>
2017-01-22 13:50:20 +0100

    sound and vfx

---
+++ Assets/Scripts/SoundScenePersistence.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScenePersistence : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+			Object.DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
